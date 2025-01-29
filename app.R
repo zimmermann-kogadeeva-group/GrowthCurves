4b260e4f252acbd3a916d90a8bfcb8dadfcc9697
Bartosz Bartmanski <bartosz.bartmanski@embl.de>
2025-01-29 20:44:24 +0100

    Added metadata upload part and filtering based on multiple columns in the table.

--- app.R
+++ app.R
@@ -12,122 +12,219 @@ wells <- expand.grid(col=LETTERS[1:8], row=seq(1, 12)) %>%
 
 # Define UI for app that draws a histogram ----
 ui <- page_sidebar(
-  # App title ----
-  title="Growth curves",
+    # App title ----
+    title="Growth curves",
 
-  # Sidebar panel for inputs ----
-  sidebar=sidebar(
-    fileInput("file1", "Choose a File:", multiple=F, accept=c(".xlsx")), 
+    # Sidebar panel for inputs ----
+    sidebar=sidebar(
+        wellPanel(
+            h4("Data"),
+                
+            fileInput("file1", "Choose a File:", multiple=F, accept=c(".xlsx")), 
 
-    selectizeInput(
-      "sheet_names", "Sheets from excel table:", choices=NULL, multiple=TRUE
+            selectizeInput(
+                "sheet_names", "Sheets from excel table:", choices=NULL, multiple=TRUE
+            ),
+
+            selectizeInput(
+                "blank_wells", "Blank wells:", choices=wells, multiple=TRUE
+            ),
+
+            selectizeInput(
+                "normalize_over", 
+                "Mean blanks over:", 
+                selected="time_elapsed_min",
+                choices=c("time_elapsed_min", "row", "col", "well", "plate"),
+                multiple=TRUE
+            ),
+
+            actionButton("open_given_sheets", "open given sheets"),
+        ),
+
+        wellPanel(
+            h4("Plotting"),
+
+            radioButtons(
+                inputId="shared_axes",
+                label="Y-axis:",
+                choices=list(
+                    "Shared"=T,
+                    "Independant"=F
+                )
+            ),
+
+            radioButtons(
+                inputId="lines",
+                label="Plot type:",
+                choices=list(
+                    "Individual lines"=T,
+                    "Mean +/- SD"=F
+                )
+            ),
+
+            fileInput("file_metadata", "Metadata:", accept=c(".xlsx")), 
+
+            selectizeInput(
+              "filter_col", "Column to filter by:", choices=NULL
+            ),
+
+            selectizeInput(
+              "filter_val", "Value to filter by:", choices=NULL
+            ),
+
+
+        ),
     ),
 
-    selectizeInput(
-      "blank_wells", "Blank wells:", choices=wells, multiple=TRUE
-    ),
-
-    selectizeInput(
-      "normalize_over", 
-      "Mean blanks over:", 
-      selected="time_elapsed_min",
-      choices=c("time_elapsed_min", "row", "col", "well", "plate"),
-      multiple=TRUE
-    ),
-
-    radioButtons(
-        inputId="shared_axes",
-        label="Y-axis:",
-        choices=list(
-            "Shared"=T,
-            "Independant"=F
-        )
-    ),
-
-    radioButtons(
-        inputId="lines",
-        label="Plot type:",
-        choices=list(
-            "Individual lines"=T,
-            "Mean +/- SD"=F
-        )
-    ),
-
-    actionButton("open_given_sheets", "open given sheets"),
-  ),
-
-  plotOutput("plot")
+    plotOutput("plot")
 )
 
 # Define server logic required to draw a histogram ----
 server <- function(input, output, session) {
+    v <- reactiveValues(df=NULL)
 
-  sheet_choice <- observeEvent(input$file1, {
+    sheet_choice <- observeEvent(input$file1, {
 
-    req(input$file1)
-    inFile <- input$file1
+        req(input$file1)
+        inFile <- input$file1
 
-    # Read in the sheet names
-    vars <- readxl::excel_sheets(inFile$datapath)
+        # Read in the sheet names
+        vars <- readxl::excel_sheets(inFile$datapath)
 
-    # Update select input immediately after clicking on the action button. 
-    updateSelectizeInput(session, "sheet_names", "Sheets from excel table", choices=vars, server=T)
-  })
+        # Update select input immediately after clicking on the action button. 
+        updateSelectizeInput(
+            session, 
+            "sheet_names", 
+            "Sheets from excel table",
+            choices=vars, 
+            server=T
+        )
+    })
 
-  get_sheets <- eventReactive(input$open_given_sheets, {
-    # Check the input has been given
-    req(input$sheet_names)
-    req(input$file1)
+    get_sheets <- observeEvent(input$open_given_sheets, {
+        # Check the input has been given
+        req(input$sheet_names)
+        req(input$file1)
 
-    blank_wells <- input$blank_wells
-    normalize_over <- input$normalize_over
+        blank_wells <- input$blank_wells
+        normalize_over <- input$normalize_over
 
-    # Read in the OD data
-    df <- read_od_data(
-        input$file1$datapath, 
-        sheet=input$sheet_names, 
-        blank_wells=well %in% blank_wells,
-        normalize_over=normalize_over
-    )
+        # Read in the OD data
+        v$df <- read_od_data(
+            input$file1$datapath, 
+            sheet=input$sheet_names, 
+            blank_wells=well %in% blank_wells,
+            normalize_over=normalize_over
+        )
 
-    # Add metadata for additional files, if any
-    for (f in input$file_metadata$datapath) {
-        df <- dplyr::left_join(df, readxl::read_excel(f))
-    }
-    df
-  })
-    
-  output$plot <- renderPlot({
-    df <- get_sheets()
+        vars <- v$df %>% 
+            dplyr::select(-c("time_elapsed_min", "OD", "norm_OD", "mean_OD_blank")) %>% 
+            colnames()
 
-    if (input$lines) {
-        g <- df %>%
-        ggplot(aes(x=time_elapsed_min, y=norm_OD, color=plate)) + 
-            geom_point(size=0.5) +
-            geom_line(aes(color=plate))
-    } else {
-        df_means <- df %>%
-        dplyr::group_by(time_elapsed_min, well) %>% 
-        dplyr::summarize(mean_norm_OD = mean(norm_OD), sd_norm_OD = sd(norm_OD)) %>%
-        dplyr::ungroup() %>%
-        dplyr::mutate(lb=mean_norm_OD - sd_norm_OD, ub=mean_norm_OD + sd_norm_OD) %>%
-        dplyr::full_join(df, by=c("time_elapsed_min", "well"))
+        updateSelectizeInput(
+            session, 
+            "filter_col", 
+            "Column to filter by:",
+            choices=c("", vars),
+            selected=NULL
+        )
+    })
 
-        g <- df_means %>%
-        ggplot(aes(x=time_elapsed_min)) + 
-            geom_ribbon(aes(ymin=lb, ymax=ub), fill="grey", alpha=0.7) + 
-            geom_line(aes(y=mean_norm_OD))
+    metadata_col_choices <- observeEvent(input$file_metadata, {
+        if(is.null(v$df)) return()
 
-    }
+        df <- v$df
 
-    g + 
-        ggh4x::facet_grid2(
-            row ~ col, 
-            scales=ifelse(input$shared_axes, "fixed", "free"), 
-            independent=ifelse(input$shared_axes, "none", "y")
-        ) + 
-        theme_bw()
-  })
+        req(input$file_metadata)
+
+        inFile <- input$file_metadata$datapath
+
+        # Read in the sheet names
+        metadata_sheets <- readxl::excel_sheets(inFile)
+
+        # Add metadata for additional files, if any
+        for (sheet in metadata_sheets) {
+            df <- dplyr::left_join(df, readxl::read_excel(inFile, sheet=sheet))
+        }
+       
+        # Update reactive value
+        v$df <- df
+
+        vars <- df %>% 
+            dplyr::select(-c("time_elapsed_min", "OD", "norm_OD", "mean_OD_blank")) %>% 
+            colnames()
+
+        updateSelectizeInput(
+            session, 
+            "filter_col", 
+            "Column to filter by:",
+            choices=c("", vars),
+            selected=NULL
+        )
+
+    })
+
+    filter_val <- observeEvent(input$filter_col, {
+        # update choices for filter value
+        if(is.null(v$df)) return()
+        df <- v$df
+
+        vars <- df %>% dplyr::pull(input$filter_col) %>% unique()
+
+        updateSelectizeInput(
+            session, 
+            "filter_val", 
+            "Value to filter by:",
+            choices=c("", vars), 
+            selected=NULL
+        )
+    })
+
+    output$plot <- renderPlot({
+        if(is.null(v$df)) return()
+
+        df <- v$df
+        filter_col <- input$filter_col
+        filter_val <- input$filter_val
+        
+        # Make sure that filtering can be done and does not produce an empty
+        # table
+        if (!(is.null(filter_col) || filter_col == "") 
+            && !(is.null(filter_val) || filter_val == "")) { 
+            df_subset <- df[df[filter_col] == filter_val, ]
+            if (nrow(df_subset) > 0)
+                df <- df_subset
+        }
+
+        # Whether to plot replicates separetly or not
+        if (input$lines) {
+            g <- df %>%
+            ggplot(aes(x=time_elapsed_min, y=norm_OD, color=plate)) + 
+                geom_point(size=0.5) +
+                geom_line(aes(color=plate))
+        } else {
+            df_means <- df %>%
+            dplyr::group_by(time_elapsed_min, well) %>% 
+            dplyr::summarize(mean_norm_OD = mean(norm_OD), sd_norm_OD = sd(norm_OD)) %>%
+            dplyr::ungroup() %>%
+            dplyr::mutate(lb=mean_norm_OD - sd_norm_OD, ub=mean_norm_OD + sd_norm_OD) %>%
+            dplyr::full_join(df, by=c("time_elapsed_min", "well"))
+
+            g <- df_means %>%
+            ggplot(aes(x=time_elapsed_min)) + 
+                geom_ribbon(aes(ymin=lb, ymax=ub), fill="grey", alpha=0.7) + 
+                geom_line(aes(y=mean_norm_OD))
+
+        }
+
+        g + 
+            ggh4x::facet_grid2(
+                row ~ col, 
+                scales=ifelse(input$shared_axes, "fixed", "free"), 
+                independent=ifelse(input$shared_axes, "none", "y")
+            ) + 
+            theme_bw()
+    })
 
 }
 
