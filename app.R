6e9304d625af8a38450c395344f8198e941363f8
Bartosz Bartmanski <bartosz.bartmanski@embl.de>
2025-08-21 13:58:17 +0200

    Added fitting to exponential and logistic.

--- app.R
+++ app.R
@@ -1,9 +1,11 @@
 library(shiny)
 library(bslib)
 library(magrittr)
+library(plotly)
 library(ggplot2)
 
 source("read_od_data.R")
+source("fitting.R")
 
 
 wells <- expand.grid(col = LETTERS[1:8], row = seq(1, 12)) %>%
@@ -11,75 +13,93 @@ wells <- expand.grid(col = LETTERS[1:8], row = seq(1, 12)) %>%
   dplyr::pull(well)
 
 # Define UI for app that draws a histogram ----
+
 ui <- page_sidebar(
   # App title ----
   title = "Growth curves",
 
   # Sidebar panel for inputs ----
   sidebar = sidebar(
-    wellPanel(
-      h4("Data"),
-      fileInput(
-        "file1",
-        "Choose a file:",
-        multiple = TRUE,
-        accept = c(".xlsx", ".txt")
+    accordion(
+      accordion_panel(
+        title = "Load data",
+        fileInput(
+          "file1",
+          "Choose a file:",
+          multiple = TRUE,
+          accept = c(".xlsx", ".txt")
+        ),
+        selectizeInput(
+          "sheet_names",
+          "Choose sheets:",
+          choices = NULL,
+          multiple = TRUE
+        ),
+        selectizeInput(
+          "blank_wells",
+          "Blank wells:",
+          choices = wells,
+          multiple = TRUE
+        ),
+        selectizeInput(
+          "normalize_over",
+          "Mean blanks over:",
+          selected = "time_elapsed_min",
+          choices = c("time_elapsed_min", "row", "col", "well", "plate"),
+          multiple = TRUE
+        ),
+        actionButton("open_given_sheets", "open given sheets"),
       ),
-      selectizeInput(
-        "sheet_names",
-        "Choose sheets:",
-        choices = NULL,
-        multiple = TRUE
+      accordion_panel(
+        title = "Plotting options",
+        radioButtons(
+          inputId = "shared_axes",
+          label = "Y-axis:",
+          choices = list(
+            "Shared" = TRUE,
+            "Independant" = FALSE
+          )
+        ),
+        radioButtons(
+          inputId = "lines",
+          label = "Plot type:",
+          choices = list(
+            "Individual lines" = TRUE,
+            "Mean +/- SD" = FALSE
+          )
+        ),
+        fileInput("file_metadata", "Metadata:", accept = c(".xlsx")),
+        selectizeInput(
+          "filter_col", "Column to filter by:",
+          choices = NULL
+        ),
+        selectizeInput(
+          "filter_val", "Value to filter by:",
+          choices = NULL,
+          multiple = TRUE,
+        ),
       ),
-      selectizeInput(
-        "blank_wells",
-        "Blank wells:",
-        choices = wells,
-        multiple = TRUE
+      accordion_panel(
+        title = "Fitting options",
+        actionButton("clear_fit", "Clear fitting"),
+        radioButtons(
+          inputId = "fitting_opt",
+          label = "Model type:",
+          choices = list(
+            "Exponential" = "exp",
+            "Logistic" = "logistic"
+          ),
+          selected = character(0)
+        ),
+        uiOutput("further_fitting_opts"),
       ),
-      selectizeInput(
-        "normalize_over",
-        "Mean blanks over:",
-        selected = "time_elapsed_min",
-        choices = c("time_elapsed_min", "row", "col", "well", "plate"),
-        multiple = TRUE
-      ),
-      actionButton("open_given_sheets", "open given sheets"),
-    ),
-    wellPanel(
-      h4("Plotting"),
-      radioButtons(
-        inputId = "shared_axes",
-        label = "Y-axis:",
-        choices = list(
-          "Shared" = TRUE,
-          "Independant" = FALSE
-        )
-      ),
-      radioButtons(
-        inputId = "lines",
-        label = "Plot type:",
-        choices = list(
-          "Individual lines" = TRUE,
-          "Mean +/- SD" = FALSE
-        )
-      ),
-      fileInput("file_metadata", "Metadata:", accept = c(".xlsx")),
-      selectizeInput(
-        "filter_col", "Column to filter by:",
-        choices = NULL
-      ),
-      selectizeInput(
-        "filter_val", "Value to filter by:",
-        choices = NULL,
-        multiple = TRUE,
-      ),
-    ),
+      open = "Load data",
+      multiple = FALSE,
+    )
   ),
-  plotOutput("plot")
+  plotlyOutput("plot")
 )
 
-# Define server logic required to draw a histogram ----
 server <- function(input, output, session) {
   v <- reactiveValues(df_full = NULL, df_subset = NULL)
 
@@ -197,6 +217,7 @@ server <- function(input, output, session) {
       dplyr::pull(input$filter_col) %>%
       unique()
 
+    # TODO: clearing this should reset the view to all
     updateSelectizeInput(
       session,
       "filter_val",
@@ -204,6 +225,51 @@ server <- function(input, output, session) {
       choices = c("", vars),
       selected = NULL
     )
+  })
+
+  observeEvent(input$clear_fit, {
+    updateRadioButtons(session, "fitting_opt", selected = character(0))
+  })
+
+  output$further_fitting_opts <- renderUI({
+    if (!is.null(input$fitting_opt)) {
+      if (input$fitting_opt == "exp") {
+        sliderInput(
+          "exp_window_size",
+          "Window size:",
+          min = 0,
+          max = 50,
+          value = 5
+        )
+      } else if (input$fitting_opt == "logistic") {
+        tagList(
+          sliderInput(
+            "logit_y0",
+            "y-intercept",
+            min = 0.0,
+            max = 1.0,
+            value = c(0.0, 0.3),
+            step = 0.01
+          ),
+          sliderInput(
+            "logit_mumax",
+            "growth rate",
+            min = 0.0,
+            max = 10.0,
+            value = c(0.2, 0.6),
+            step = 0.01
+          ),
+          sliderInput(
+            "logit_K",
+            "carrying capacity",
+            min = 0.0,
+            max = 2.0,
+            value = c(0.8, 1.5),
+            step = 0.01
+          )
+        )
+      }
+    }
   })
 
   observeEvent(input$filter_val, {
@@ -222,6 +288,8 @@ server <- function(input, output, session) {
       df_subset <- df %>% dplyr::filter(!!filter_col == filter_val)
     } else if (length(filter_val) > 1) {
       df_subset <- df %>% dplyr::filter(!!filter_col %in% filter_val)
+    } else {
+      df_subset <- df
     }
 
     # Make sure that filtering can be done and does not produce an empty
@@ -231,34 +299,115 @@ server <- function(input, output, session) {
     }
   })
 
-  output$plot <- renderPlot({
+  output$plot <- renderPlotly({
     if (is.null(v$df_subset)) {
       return()
     }
 
-    df <- v$df_subset
+    withProgress(message = "Making plot", {
+      df <- v$df_subset
 
-    # Whether to plot replicates separetly or not
-    if (input$lines) {
-      g <- df %>%
-        ggplot(aes(x = time_elapsed_min, y = norm_OD, color = plate)) +
-        geom_point(size = 0.5) +
-        geom_line(aes(color = plate))
-    } else {
-      g <- df %>%
-        add_mean_and_sd() %>%
-        ggplot(aes(x = time_elapsed_min)) +
-        geom_ribbon(aes(ymin = lb, ymax = ub), fill = "grey", alpha = 0.7) +
-        geom_line(aes(y = mean_norm_OD))
-    }
+      incProgress(1 / 4, detail = "Setting up base plot")
 
-    g +
-      ggh4x::facet_grid2(
-        row ~ col,
-        scales = ifelse(input$shared_axes, "fixed", "free"),
-        independent = ifelse(input$shared_axes, "none", "y")
-      ) +
-      theme_bw()
+      # Whether to plot replicates separetly or not
+      if (input$lines) {
+        g <- df %>%
+          ggplot(aes(x = time_elapsed_min, y = norm_OD, color = plate)) +
+          geom_point(size = 0.5) +
+          geom_line(aes(color = plate))
+      } else {
+        g <- df %>%
+          add_mean_and_sd() %>%
+          ggplot(aes(x = time_elapsed_min)) +
+          geom_ribbon(aes(ymin = lb, ymax = ub), fill = "grey", alpha = 0.7) +
+          geom_line(aes(y = mean_norm_OD))
+      }
+
+      incProgress(2 / 4, detail = "Setting up facets")
+
+      g <- g +
+        ggh4x::facet_grid2(
+          row ~ col,
+          scales = ifelse(input$shared_axes, "fixed", "free"),
+          independent = ifelse(input$shared_axes, "none", "y")
+        ) +
+        theme_bw()
+
+      if (!is.null(input$fitting_opt)) {
+        df_pred <- data.frame()
+
+        if (input$fitting_opt == "exp") {
+          # Fitting stuff - here for the moment
+          spar <- 0.5
+
+          if (nrow(df %>% dplyr::filter(norm_OD < 0)) > 0) {
+            showNotification(
+              paste0(
+                "Some wells and replicates have negative values!\n",
+                "No fitting will be done in these cases."
+              )
+            )
+          }
+          fit_exp <- growthrates::all_easylinear(
+            norm_OD ~ time_elapsed_min | row + col + plate,
+            data = {
+              df %>%
+                dplyr::group_by(well, plate) %>%
+                dplyr::filter(!any(norm_OD < 0)) %>%
+                dplyr::ungroup()
+            },
+            h = input$exp_window_size,
+            spar = spar
+          )
+
+          df_pred <- fit_exp %>%
+            all_pred_linear(time_elapsed_min = "time", norm_OD = "y") %>%
+            dplyr::filter(norm_OD < max(df$norm_OD))
+        } else if (input$fitting_opt == "logistic") {
+          p <- c(
+            y0 = mean(input$logit_y0),
+            mumax = mean(input$logit_mumax),
+            K = mean(input$logit_K)
+          )
+          lower <- c(
+            y0 = input$logit_y0[1],
+            mumax = input$logit_mumax[1],
+            K = input$logit_K[1]
+          )
+          upper <- c(
+            y0 = input$logit_y0[2],
+            mumax = input$logit_mumax[2],
+            K = input$logit_K[2]
+          )
+
+          fit_logit <- growthrates::all_growthmodels(
+            norm_OD ~ growthrates::grow_logistic(time_elapsed_min, parms) | row + col + plate,
+            data = df,
+            p = p,
+            lower = lower,
+            upper = upper,
+          )
+
+          df_pred <- fit_logit %>%
+            all_pred_nonlinear(time_elapsed_min = "time", norm_OD = "y")
+        }
+
+        g <- g + geom_line(
+          aes(group = plate),
+          data = df_pred,
+          color = "black",
+          linewidth = 1.6
+        ) +
+          geom_line(data = df_pred, linetype = "dashed", linewidth = 1.0)
+        # TODO: download button for df_pred
+      }
+
+      incProgress(3 / 4, detail = "Finishing figure")
+
+      g %>%
+        ggplotly() %>%
+        return()
+    })
   })
 }
 
---
+++ fitting.R
@@ -0,0 +1,52 @@
+library(magrittr)
+
+normalize <- function(data, value = 0.01) {
+  data %>%
+    dplyr::mutate(
+      dplyr::across(.cols = -Time, .fns = ~ (value - .[Time == 0]) + .)
+    )
+}
+
+get_pred_linear <- function(fit) {
+  fit_obs <- growthrates::obs(fit)
+  coeffs <- growthrates::coef(fit)
+
+  fit@FUN(
+    fit_obs$time,
+    c(y0 = unname(coeffs["y0_lm"]), mumax = unname(coeffs["mumax"]))
+  ) %>%
+    as.data.frame()
+}
+
+get_pred_nonlinear <- function(fit) {
+  fit_obs <- growthrates::obs(fit)
+  coeffs <- growthrates::coef(fit)
+
+  fit@FUN(fit_obs$time, coeffs) %>% as.data.frame()
+}
+
+all_pred_linear <- function(fits, ...) {
+  fits@fits %>%
+    purrr::imap(~ {
+      get_pred_linear(.x) %>% dplyr::mutate(grouping = .y)
+    }) %>%
+    dplyr::bind_rows() %>%
+    tidyr::separate_wider_delim(
+      grouping,
+      delim = ":", names = fits@grouping
+    ) %>%
+    dplyr::rename(...)
+}
+
+all_pred_nonlinear <- function(fits, ...) {
+  fits@fits %>%
+    purrr::imap(~ {
+      get_pred_nonlinear(.x) %>% dplyr::mutate(grouping = .y)
+    }) %>%
+    dplyr::bind_rows() %>%
+    tidyr::separate_wider_delim(
+      grouping,
+      delim = ":", names = fits@grouping
+    ) %>%
+    dplyr::rename(...)
+}
